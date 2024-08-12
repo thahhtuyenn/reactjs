b0633b906829719e4d017b3bba309f82d36f805c
thahhtuyenn <thanhtuyen9623@gmail.com>
2024-08-12 13:01:29 +0700

    tao project reactjs

---
+++ src/views/jobs/Job.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Job extends React.Component {
+  render() {
+    return (
+      <>
+        <div>List job with react js</div>
+      </>
+    );
+  }
+}
+
+export default Job;
---
+++ src/views/todos/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Todo extends React.Component {
+  render() {
+    return (
+      <>
+        <div>List todo with react js</div>
+      </>
+    );
+  }
+}
+
+export default Todo;
